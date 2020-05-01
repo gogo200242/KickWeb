b6cce1e2afd8430e0f39a065c8689a9fd371093a
Hugo Surrel <60435919+gogo200242@users.noreply.github.com>
2020-05-01 18:11:26 +0200

    Premier commit

---
+++ accueil.php
@@ -0,0 +1,45 @@
+
+<html>
+<head>
+	<title>Accueil</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/mainaccueil.css"/>
+	<link rel="stylesheet" type="text/css" href="css/reset.css"/>
+</head>
+<body>
+	<header class="top">
+	
+	</header>
+
+	<section>
+		<a href="../actualitees.html"><img class="logo" src="../img/logo mutuelle.jpg" /></a>
+	</section>
+
+	<header class="head">
+		
+		<nav class="wrapper">
+			<ul>
+				<li>
+					<a href="../magazine.php" title="Magazine">Magazine</a>
+					<ul class="sub-menu">
+						<li>
+							<a href="../avril2020.php" title="avril2020">Avril 2020</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="../boutique.php" title="Boutique">Boutique</a>
+				</li>
+				<li>
+					<a href="../contact.php" title="Contact">Nous contacter</a>
+				</li>
+			</ul>
+		</nav>
+
+	</header> 
+
+	<embed src="magazine/avril2020.pdf" width="600px" height="800px"></embed>
+
+
+</body>
+</html>
